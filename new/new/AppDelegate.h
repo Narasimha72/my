192da5d484c4104a9564722a51c6944cec7ff1b8
Nuvvala <narasimha.nuvvala@gmail.com>
2015-04-26 18:44:47 -0700

    singleView Controller

---
+++ new/new/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  new
+//
+//  Created by Nuvvala on 4/26/15.
+//  Copyright (c) 2015 Narasimha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ new/new/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  new
+//
+//  Created by Nuvvala on 4/26/15.
+//  Copyright (c) 2015 Narasimha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
