e426e96fb08e969b661fb7ca026a5eb306a60e7a
Nuvvala <narasimha.nuvvala@gmail.com>
2015-04-26 20:08:47 -0700

    new change-view

--- new/new/ViewController.m
+++ new/new/ViewController.m
@@ -18,6 +18,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:YES];
+
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
